ba386b497847becbd4639ebc79e5963de45659c8
TuoTuo0710 <55468288+TuoTuo0710@users.noreply.github.com>
2019-12-17 11:57:18 +0800

    Add files via upload

---
+++ button_Transition.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+
+public class button_Transition : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,IPointerDownHandler,IPointerUpHandler,IPointerClickHandler
+{
+    public Color32 m_normal = Color.white;
+    public Color32 m_hover = Color.grey;
+    public Color32 m_down = Color.white;
+    public UnityEvent button_pinch;
+
+
+    private Image m_image;
+
+    private void Awake()
+    {
+        m_image = GetComponent<Image>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("enter");
+
+        m_image.color = m_hover;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("Exit");
+
+        m_image.color = m_normal;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print("Down");
+
+        m_image.color = m_down;
+        button_pinch.Invoke();
+
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        print("Up");
+        m_image.color = m_normal;
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("Click");
+
+        m_image.color = m_normal;
+    }
+}
